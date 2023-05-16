9d5190b2d5e1bd22530b1ff2251f5f0b4eda9dad
Praneeth <saimallampalli1707@gmail.com>
2023-05-16 20:55:13 +0000

    initial commit

---
+++ src/components/AssetDisplay.tsx
@@ -0,0 +1,10 @@
+import AssetInfo from "./AssetInfo";
+
+export default function AssetDisplay() {
+  return (
+    <>
+      <div>Asset Image is displayed here</div>
+      <AssetInfo />
+    </>
+  );
+}
